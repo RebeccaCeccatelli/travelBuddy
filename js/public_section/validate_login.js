70f581f627041c2aa0f7043afc8efe55a88f5794
rebeccaceccatelli <rebeccaceccatelli@gmail.com>
2023-09-14 16:06:58 +0200

    Set buttons in popups inside map, fixed account_details.html. Saving before beginning code refactoring.

--- js/public_section/validate_login.js
+++ js/public_section/validate_login.js
@@ -1,37 +1,42 @@
- const allowedUsers = [
-     { email: "rebeccaceccatelli@gmail.com", password: "Pianoforte2000!" },
-     { email: "trattorialafontana@gmail.com", password: "wlaciccia4ever" }
- ];
+const loginForm = document.getElementById("loginForm");
 
- const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
- loginForm.addEventListener("submit", function (event) {
-     event.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-     const email = document.getElementById("email").value;
-     const password = document.getElementById("password").value;
+    fetch("../../json_files/registered_clients.json")
+        .then(response => response.json())
+        .then(allowedUsers => {
+            let accessAllowed = false;
+            let userType = "";
 
-     let accessAllowed = false;
+            for (const user of allowedUsers) {
+                if (email === user.email && password === user.password) {
+                    accessAllowed = true;
+                    userType = user.type;
+                    currentUser = user;
+                    break;
+                }
+            }
 
-     for (const user of allowedUsers) {
-         if (email === user.email && password === user.password) {
-             accessAllowed = true;
-             currentUser = user;
-             break;
-         }
-     }
+            if (accessAllowed) {
+                alert("Login successful!");
+                if (userType === "user") {
+                    window.location.href = "../../html/user_section/user_dashboard.html";
+                } else if (userType === "business") {
+                    window.location.href = "../../html/business_section/business_dashboard.html";
+                } else {
+                    alert("Unknown user type. Please try again.");
+                }
+            } else {
+                alert("Access denied. Please try again.");
+            }
 
-     if (accessAllowed) {
-         alert("Login successful!");
-         if (currentUser === allowedUsers[0]){
-             window.location.href = "../../html/user_section/user_dashboard.html";
-         }
-         else{
-             window.location.href = "../../html/business_section/business_dashboard.html";
-         }
-     } else {
-         alert("Access denied. Please try again.");
-     }
-
-     loginForm.reset();
- });
+            loginForm.reset();
+        })
+        .catch(error => {
+            console.error("Error loading registered_clients.json: ", error);
+        });
+});
