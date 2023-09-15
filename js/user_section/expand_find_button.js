4e9af6fa65670b8958407cbd4fbe5df46c5aa322
rebeccaceccatelli <rebeccaceccatelli@gmail.com>
2023-09-15 16:08:34 +0200

    Created subscriptions.html.

--- js/user_section/expand_find_button.js
+++ js/user_section/expand_find_button.js
@@ -1,5 +1,6 @@
-document.getElementById("findAndReserveButton").addEventListener("click", function() {
-    var expandable = document.getElementById("expandable");
+document.getElementById("expandableButton").addEventListener("click", function() {
+    var expandable = document.getElementById("expandable")
+        || document.getElementById("expandable_display_by_default");
     if (expandable.style.display === "none") {
         expandable.style.display = "block";
     } else {
