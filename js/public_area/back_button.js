1de54f52e0beda0fc6a5c5d17a404d2311a258c7
rebeccaceccatelli <rebeccaceccatelli@gmail.com>
2023-09-18 23:57:12 +0200

    Added back button everywhere and handled logout (impossibility to go back to personal dashboard through browser back arrow).

---
+++ js/public_area/back_button.js
@@ -0,0 +1,7 @@
+document.getElementById("back").addEventListener("click", function() {
+    if (document.referrer && !window.location.href.includes("dashboard")) {
+        window.location.href = document.referrer;
+    } else {
+        window.location.href = "../../public_area/homepage.html";
+    }
+});
